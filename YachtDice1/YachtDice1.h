17c79ef3fddf6c8285681d83c4d6c4c208ad2a12
HJ5323 <167291731+HJ5323@users.noreply.github.com>
2024-06-10 18:02:55 +0900

    Add project files.

---
+++ YachtDice1/YachtDice1.h
@@ -0,0 +1,32 @@
+﻿
+// YachtDice1.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CYachtDice1App:
+// See YachtDice1.cpp for the implementation of this class
+//
+
+class CYachtDice1App : public CWinApp
+{
+public:
+	CYachtDice1App();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CYachtDice1App theApp;
